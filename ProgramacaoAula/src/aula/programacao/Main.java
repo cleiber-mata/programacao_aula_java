31fa97dd48cc562024a6627b63e096096348e6c6
cleiber-mata <cleiber.mata@gmail.com>
2026-05-17 15:13:11 -0300

    Teste

--- ProgramacaoAula/src/aula/programacao/Main.java
+++ ProgramacaoAula/src/aula/programacao/Main.java
@@ -25,7 +25,7 @@ public class Main {
 		
 		Professor pedro = new Professor(); 
 		pedro.setNome("Pedro"); 
-		pedro.setSobrenome("Silva"); 
+		pedro.setSobrenome("Paulo"); 
 		pedro.setCpf("11122233311"); 
 		pedro.andar(); 
 		pedro.parar(); 
